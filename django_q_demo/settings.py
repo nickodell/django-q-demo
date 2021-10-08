9c094a47e1f36c626f2843da64cdaf700d6abd7b
Nick ODell <nickodell@gmail.com>
2021-10-08 16:13:31 -0600

    Add cache settings

--- django_q_demo/settings.py
+++ django_q_demo/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 Q_CLUSTER = {
-    # 'workers': 4,
+    'workers': 1,
     'timeout': 50,
     'retry': 60,
     'redis': {
@@ -142,4 +142,15 @@ Q_CLUSTER = {
         'charset': 'utf-8',
         'errors': 'strict',
     },
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
 }
